4bf6ae61c50df88016b2c64c86eb6115c85742ab
Cezar Pendarovski <cezar.pendarovski@roslin.ed.ac.uk>
2017-12-20 14:33:28 +0000

    Set up the database connection

--- dataportal/settings.py
+++ dataportal/settings.py
@@ -10,6 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
+# The file parameters.py must be present in the site-packages directory
+# of the virtual environment. Inside the dictionary credentials must be
+# present holding all secret parameters. (Cezar)
+from parameters import credentials
+
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -20,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fsqc9mreo4*4g5okc16#l=qye%(%6kc#ulgx-a+z%^kzc_@=!#'
+SECRET_KEY = credentials.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +81,12 @@ WSGI_APPLICATION = 'dataportal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': credentials.get('DB_NAME'),
+        'USER': credentials.get('DB_USER'),
+        'PASSWORD': credentials.get('DB_PASSWORD'),
+        'HOST': credentials.get('DB_HOST'),
+        'PORT': credentials.get('DB_PORT')
     }
 }
 
