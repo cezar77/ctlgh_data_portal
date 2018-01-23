ca2af38307e37d46fc1d0ada0563e146b445576f
Cezar Pendarovski <cezar.pendarovski@roslin.ed.ac.uk>
2018-01-23 14:55:57 +0000

    Add basic admin for sheep

--- dataportal/modules/sheep/admin.py
+++ dataportal/modules/sheep/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+from django.contrib.gis import admin
 
-# Register your models here.
+from .models import Population, Sampling, Animal
+
+
+class SamplingAdmin(admin.GeoModelAdmin):
+    list_display = ['date', 'population', 'locality']
+    num_zoom = 6
+    modifiable = False
+
+admin.site.register(Population)
+admin.site.register(Sampling, SamplingAdmin)
+admin.site.register(Animal)
