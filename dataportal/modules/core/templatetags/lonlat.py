206aecaf67abefcbb160206e4042ac8b67ea736e
Cezar Pendarovski <cezar.pendarovski@roslin.ed.ac.uk>
2018-02-02 19:26:12 +0000

    Add tests for lonlat template filters

--- dataportal/modules/core/templatetags/lonlat.py
+++ dataportal/modules/core/templatetags/lonlat.py
@@ -4,23 +4,27 @@ register = template.Library()
 
 @register.filter
 def longitude(value):
+    if value > 180 or value < -180:
+        return value
     if value > 0:
         side = 'E'
     elif value < 0:
         side = 'W'
     else:
         side = ''
-    return get_degree(value, side)
+    return get_degree(abs(value), side)
 
 @register.filter
 def latitude(value):
+    if value > 90 or value < -90:
+        return value
     if value > 0:
         side = 'N'
     elif value < 0:
         side = 'S'
     else:
         side = ''
-    return get_degree(value, side)
+    return get_degree(abs(value), side)
 
 def get_degree(value, side):
     result = u'{deg}\u00b0{min}\'{sec}"{side}'
@@ -30,5 +34,7 @@ def get_degree(value, side):
     if second >= 60:
         second = 0
         minute += 1
+    minute = str(minute).zfill(2)
+    second = str(second).zfill(2)
     return result.format(side=side, deg=degree, min=minute, sec=second)
 
--- dataportal/modules/core/tests.py
+++ dataportal/modules/core/tests.py
@@ -1,3 +1,72 @@
+from django.template import Template, Context
 from django.test import TestCase
 
-# Create your tests here.
+
+class LonlatTagsTest(TestCase):
+    lon_template = Template('{% load lonlat %}{{ gps|longitude }}')
+    lat_template = Template('{% load lonlat %}{{ gps|latitude }}')
+
+    def test_longitude_east(self):
+        """
+        Longitude of Johannesburg
+        """
+        gps = 28.04556
+        t = self.lon_template.render(Context({'gps': gps}))
+        self.assertIn(u'\u00b0', t)
+        self.assertEqual('28\u00b002&#39;44&quot;E', t)
+
+    def test_longitude_west(self):
+        """
+        Longitude of Edinburgh
+        """
+        gps = -3.188889
+        t = self.lon_template.render(Context({'gps': gps}))
+        self.assertEqual('3\u00b011&#39;20&quot;W', t)
+
+    def test_latitude_north(self):
+        """
+        Latitude of Edinburgh
+        """
+        gps = 55.95306
+        t = self.lat_template.render(Context({'gps': gps}))
+        self.assertEqual('55\u00b057&#39;11&quot;N', t)
+
+    def test_latitude_south(self):
+        """
+        Latitude of Johannesburg
+        """
+        gps = -26.20444
+        t = self.lat_template.render(Context({'gps': gps}))
+        self.assertEqual('26\u00b012&#39;16&quot;S', t)
+
+    def test_zero_longitude(self):
+        """
+        Zero longitude
+        """
+        gps = 0
+        t = self.lon_template.render(Context({'gps': gps}))
+        self.assertEqual('0\u00b000&#39;00&quot;', t)
+
+    def test_zero_latitude(self):
+        """
+        Zero latitude
+        """
+        gps = 0
+        t = self.lat_template.render(Context({'gps': gps}))
+        self.assertEqual('0\u00b000&#39;00&quot;', t)
+
+    def test_invalid_longitude(self):
+        """
+        Invalid longitude
+        """
+        gps = -200.25
+        t = self.lon_template.render(Context({'gps': gps}))
+        self.assertEqual('-200.25', t)
+
+    def test_invalid_latitude(self):
+        """
+        Invalid latitude
+        """
+        gps = 100.83333
+        t = self.lat_template.render(Context({'gps': gps}))
+        self.assertEqual('100.83333', t)
