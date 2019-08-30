6a350a2df1422844e37a4127112ed772281ac01c
Cezar Pendarovski <cezar.pendarovski@roslin.ed.ac.uk>
2019-08-30 08:58:43 +0100

    Cache the homepage

--- dataportal/modules/core/views.py
+++ dataportal/modules/core/views.py
@@ -1,8 +1,11 @@
 from django.views.generic import ListView
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 
 from dataportal.modules.animals.models import Species
 
 
+@method_decorator(cache_page(30*24*60*60), name='dispatch')
 class HomeView(ListView):
     model = Species
     context_object_name = 'species'
